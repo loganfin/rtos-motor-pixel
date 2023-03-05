d81f02d6620601d1bfd3aaeab1ce16844187bd5c
Logan Finley <finley4.logan@gmail.com>
2023-03-05 09:05:38 -0800

    Button 1 functions as expected

--- src/buttons.c
+++ src/buttons.c
@@ -14,7 +14,7 @@
 const uint buttons_s1_pin = 19;
 const uint buttons_s2_pin = 9;
 const uint buttons_s3_pin = 8;
-const uint buttons_debounce_delay = 110 / portTICK_PERIOD_MS;
+const uint buttons_debounce_delay = 160 / portTICK_PERIOD_MS;
 
 SemaphoreHandle_t xButton1Semaphore;
 SemaphoreHandle_t xButton2Semaphore;
@@ -33,6 +33,10 @@ void buttons_init()
     gpio_set_irq_enabled_with_callback(buttons_s1_pin, GPIO_IRQ_EDGE_RISE, 1, isr_buttons);
     gpio_set_irq_enabled(buttons_s2_pin, GPIO_IRQ_EDGE_RISE, 1);
     gpio_set_irq_enabled(buttons_s3_pin, GPIO_IRQ_EDGE_RISE, 1);
+
+    gpio_init(20);
+    gpio_set_dir(20, GPIO_OUT);
+    gpio_put(20, 1);
 }
 
 void isr_buttons(uint gpio, uint32_t events)
@@ -58,41 +62,152 @@ void isr_buttons(uint gpio, uint32_t events)
 void vButton1()
 {
     uint count = 0;
+    TickType_t input_frame = 2 * configTICK_RATE_HZ;
+    TickType_t start = 0;
+    TickType_t end = 0;
     vSemaphoreCreateBinary(xButton1Semaphore);
 
     while(true) {
-        xSemaphoreTake(xButton1Semaphore, portMAX_DELAY);
-        vTaskDelay(buttons_debounce_delay);
-        xSemaphoreTake(xButton1Semaphore, 0);
-        printf("button s1 has been pressed %d times\n", count);
-        count++;
+        // debounce
+        // if the first press, start the timer
+        // increment count
+        // if end < time, try and take the semaphore again
+        // else if end > time, switch statement to handlers
+
+        // if the timer is started, don't block on semaphore
+
+        //  if the timer is started:
+        //      try to take the semaphore without blocking
+        //  if the timer is not started:
+        //      block on semaphore
+
+
+        if (start != 0) {
+            end = xTaskGetTickCount() - start;
+            if (xSemaphoreTake(xButton1Semaphore, 0) == pdTRUE) {
+                // debounce
+                vTaskDelay(buttons_debounce_delay);
+                xSemaphoreTake(xButton1Semaphore, 0);
+
+                // check the current count
+                if (count == 0) {
+                    start = xTaskGetTickCount();
+                }
+                count++;
+            }
+        }
+        else if (start == 0) {
+            if (xSemaphoreTake(xButton1Semaphore, portMAX_DELAY) == pdTRUE) {
+                // debounce
+                vTaskDelay(buttons_debounce_delay);
+                xSemaphoreTake(xButton1Semaphore, 0);
+
+                // check the current count
+                if (count == 0) {
+                    start = xTaskGetTickCount();
+                }
+                count++;
+            }
+        }
+
+        // maybe move this above the button handler?
+        if (end > input_frame) {
+            start = 0;
+            end = 0;
+
+            switch (count) {
+                case 1:
+                    printf("case 1 press. actual press %d\n", count);
+                    break;
+                case 2:
+                    printf("case 2 press. actual press %d\n", count);
+                    break;
+                case 3:
+                    printf("case 3 press. actual press %d\n", count);
+                    break;
+                case 4:
+                    printf("case 4 press. actual press %d\n", count);
+                    break;
+                default:
+                    printf("case default press. actual press %d\n", count);
+                    break;
+            }
+            count = 0;
+        }
     }
 }
 
 void vButton2()
 {
     uint count = 0;
+    TickType_t input_frame = 2 * configTICK_RATE_HZ;
+    TickType_t start = 0;
+    TickType_t end = 0;
     vSemaphoreCreateBinary(xButton2Semaphore);
 
     while(true) {
-        xSemaphoreTake(xButton2Semaphore, portMAX_DELAY);
-        vTaskDelay(buttons_debounce_delay);
-        xSemaphoreTake(xButton2Semaphore, 0);
-        printf("button s2 has been pressed %d times\n", count);
-        count++;
+        /*
+         * On button press:
+         * give semaphore
+         *
+         * increment count
+         */
+        if (xSemaphoreTake(xButton2Semaphore, portMAX_DELAY) == pdTRUE) {
+            // debounce the button
+            vTaskDelay(buttons_debounce_delay);
+            xSemaphoreTake(xButton2Semaphore, 0);
+
+            // count the number of button presses within 2 seconds
+            // what happens if the button is pressed within 2 seconds of startup?
+            end = xTaskGetTickCount() - start;
+            if (end > input_frame) {
+                start = xTaskGetTickCount();
+                count = 0;
+            }
+            count++;
+
+            printf("button s2 has been pressed %d times\n", count);
+            printf("start tick: %d\n", start);
+            printf("end tick: %d\n", end);
+            printf("input_frame tick: %d\n\n", input_frame);
+        }
     }
 }
 
 void vButton3()
 {
     uint count = 0;
+    TickType_t input_frame = 2 * configTICK_RATE_HZ;
+    TickType_t start = 0;
+    TickType_t end = 0;
     vSemaphoreCreateBinary(xButton3Semaphore);
 
     while(true) {
-        xSemaphoreTake(xButton3Semaphore, portMAX_DELAY);
-        vTaskDelay(buttons_debounce_delay);
-        xSemaphoreTake(xButton3Semaphore, 0);
-        printf("button s3 has been pressed %d times\n", count);
-        count++;
+        if (xSemaphoreTake(xButton3Semaphore, portMAX_DELAY) == pdTRUE) {
+            /*
+               if (end > input_frame) {
+               start = xTaskGetTickCount();
+               end = start;
+               count = 0;
+               }
+               */
+
+            vTaskDelay(buttons_debounce_delay);
+            xSemaphoreTake(xButton2Semaphore, 0);
+            count++;
+
+            end = xTaskGetTickCount() - start;
+            if (end > input_frame) {
+                start = xTaskGetTickCount();
+                end = start;
+                count = 0;
+                continue;
+            }
+
+            printf("button s2 has been pressed %d times\n", count);
+            printf("start tick: %d\n", start);
+            printf("end tick: %d\n", end);
+            printf("input_frame tick: %d\n\n", input_frame);
+        }
     }
 }
